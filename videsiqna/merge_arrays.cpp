4ec40999617e5f4ba4627854908f198b50040ba8
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2026-07-25 19:36:50 +0530

    Added the important merge array combinatorics problem.

--- videsiqna/merge_arrays.cpp
+++ videsiqna/merge_arrays.cpp
@@ -1,3 +1,11 @@
+// to find the number of ways such that
+// array a and array b
+// after merging them their internal ordering should be intact
+// the formula becomes
+// (a+b)C(b) or (a+b)C(a)
+// here a and b are the sizes of 2 arrays
+
+// editorial- https://claude.ai/share/29346ec8-f5cd-4593-9bdc-50b1e8ebe8c2
 
 #include <bits/stdc++.h>
 using namespace std;
